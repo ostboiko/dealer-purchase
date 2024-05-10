8c9c39b0e95fc3bacbc1d97fef855700ad30a5d5
Ostap <ostapbojko04@gmail.com>
2024-05-10 17:17:14 +0300

    Customization front-end

--- dealer_service/settings.py
+++ dealer_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'catalog.context_processors.cfg_assets_root',
             ],
         },
     },
@@ -125,6 +126,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+ASSETS_ROOT = "static/assets"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
