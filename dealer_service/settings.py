fd7236a03f15bf85614bfde51e960a8ad98ee14c
Ostap <ostapbojko04@gmail.com>
2024-05-15 18:22:02 +0300

    Delete redundant information

--- dealer_service/settings.py
+++ dealer_service/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'catalog.context_processors.cfg_assets_root',
+                'catalog.context_processors.cfg_assets_root',
             ],
         },
     },
