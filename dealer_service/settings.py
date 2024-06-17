6cafb2f64136ac866fda7af8f89eed09c26f6d9e
Ostap <ostapbojko04@gmail.com>
2024-06-17 22:33:30 +0300

    add dealer-purchase.onrender.com to ALLOWED_HOSTS

--- dealer_service/settings.py
+++ dealer_service/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "dealer-purchase.onrender.com"]
 
 
 # Application definition
